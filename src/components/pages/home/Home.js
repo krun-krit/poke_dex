600929709323ffd5234036aa15c9a10b2090a73d
CWN-BU4-2020-00\Thanakit.l <thanakit_lue@cmu.ac.th>
2021-12-21 17:33:17 +0700

    add api

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -3,6 +3,7 @@ import Header from "../../fragments/Header";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
 import { Card } from "antd";
+import Item from "antd/lib/list/Item";
 
 const { Meta } = Card;
 
@@ -10,13 +11,18 @@ export default function Home() {
   const [isDetail, setIsDetail] = React.useState();
 
   React.useEffect(() =>{
-    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=151`)
+    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=151`).then((res)=>{
+        setIsDetail(res.data.results)
+        console.log(res.data.results);
+    })
   },[]);
 
   return (
     <div>
       <Header />
-      
+      <div className="row">
+       
+      </div>
     </div>
   );
 }
